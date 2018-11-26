108c05a406498a5dabddbd56d9258d97bcf7793d
shahidikram0701 <shahidikram0701@gmail.com>
2018-11-26 20:24:17 +0530

    dispatch done..not tested

--- include/scheduler.h
+++ include/scheduler.h
@@ -4,6 +4,7 @@
 #include "future.h"
 #include "task.h"
 #include "shared_queue.h"
+#include <thread>
 
 typedef std::function<void()> Callback;
 
@@ -12,22 +13,29 @@ class Scheduler {
         Scheduler( const Scheduler& ) = delete;
         Scheduler& operator=( const Scheduler& ) = delete;
         shared_queue<Callback> mq_;
+        std::thread thd_;
 
     public:
 
         Scheduler(  ) {
-            dispatch();
+            thd_ = std::thread(&Scheduler::dispatch, this);
         }
 
         template<typename T>
-        void enqueue(Task<T> *t) {
-            mq_.push( []() { t->set_result( t->call(  ) ) } );
+        void enqueue( Task<T> *t ) {
+            mq_.push( [  ](  ) { t->set_result( t->call(  ) ) } );
         } 
 
 
-        void dispatch () {
-            
+        void dispatch (  ) {
+            Callback func;
+            mq_.wait_and_pop(func);
+            func();
         }
+
+        ~Scheduler(  ) {
+            thd_.join();
+        } 
 };
 
 #endif
