bcc10d4645b44692a185e99facec5466c8dc577b
shahidikram0701 <shahidikram0701@gmail.com>
2018-11-27 21:50:24 +0530

    simulate_hard_computation

--- src/client.cpp
+++ src/client.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <unistd.h>
+#include <random>
+#include <thread>
 using namespace std;
 
+std::random_device rd;
+std::mt19937 mt(rd());
+std::uniform_int_distribution<int> dist(-1000, 1000);
+auto rnd = std::bind(dist, mt);
+
+
+void simulate_hard_computation() {
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000 + rnd()));
+}
 
 #include "../include/proxy.h"
 
@@ -16,11 +27,14 @@ int main( int argc, char const *argv[] ) {
     
     // Do some other work
     //sleep(2);
+
+    simulate_hard_computation();
+
     int ans = f1->resolve();
     cout << "Task 1 : " << ans << endl;
 
     // Do some other work
-    sleep(2);
+    
     cout << "Task 2 : " << f2->resolve() << endl;
 
     // Do some other work
